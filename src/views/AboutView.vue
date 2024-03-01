e19796a661ff246a20cd9c56d30ae285f9e19a6c
Yang891231 <160811360+Yang891231@users.noreply.github.com>
2024-03-01 09:08:05 +0800

    todoList 更新

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,117 @@
-<template>
-  <div class="about">
-    
-  </div>
-</template>
+<script>
+export default {
+  data() {
+    return {
+      arr:[],
+      obj:{},
+      text:'',
+      switchBtn:'all',
+    }
+  },
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+  mounted() {
+    // localStorage.removeItem('news');
+    if(localStorage.getItem('news')){
+      this.arr = JSON.parse(localStorage.getItem('news'));
+    }
+  },
+
+  // 預處理拿到的資料,他有暫存的功能,所以我們可以拿整包資料,利用判斷式把我們資料做篩選
+  computed:{
+    selectnews(){
+      return this.arr.filter((item)=>{
+        if(this.switchBtn == 'all'){
+          return this.arr;
+        }else if(this.switchBtn == '已執行'){
+          return item.checkBox == true;
+        }else if(this.switchBtn =='未執行'){
+          return item.checkBox == false;
+        }
+        })
+      }
+    },
+  
+  methods: {
+
+  pushobj() {
+    const id = this.arr.length > 0 ? this.arr[this.arr.length -1].id : 0;
+    this.obj = {
+      checkBox : false,
+      msgSwitch : false,
+      news : this.news,
+      id : id+1,
+    };
+    
+    this.arr.push(this.obj);
+    localStorage.setItem('news', JSON.stringify(this.arr));
+    console.log(this.arr);
+  },
+  getInputValue(e) {
+    this.news = e.target.value;
+  },
+  deleteBtn(index) {
+    this.arr.splice(index,1);
+    localStorage.setItem('news', JSON.stringify(this.arr));
+  },
+
+  editBtn(index) {
+  this.arr[index].msgSwitch =! this.arr[index].msgSwitch;
+  if(this.arr[index].msgSwitch === false) {
+    this.arr[index].news = this.aaa;
   }
-}
-</style>
+  else {
+    this.aaa = this.arr[index].news; 
+  }
+  localStorage.setItem('news', JSON.stringify(this.arr));
+
+  },
+  editInput(e) {
+    this.aaa = e.target.value;
+  },
+  changecheckbox(){
+    localStorage.setItem('news', JSON.stringify(this.arr));
+  },
+  selectTab(string){
+    this.switchBtn = string;
+  }
+},
+};
+
+</script>
+<template>
+<div class="body w-screen h-screen flex justify-center items-center bg-neutral-800 text-black">
+    <div class="w-[80rem] h-[50rem] bg-stone-400 border-4 flex flex-col">
+    <div class="add m-2 ml-96">
+      <input type="text" class="add-text w-[30rem] rounded-lg text-2xl" @change="(e) => getInputValue(e)">
+      <button class="add-Todo w-20 rounded-lg text-2xl ml-8 bg-red-50" @click="pushobj()" type="button">新增</button>
+    </div>
+    <div class="search-btn m-4 text-2xl">
+      <button class="all w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" @click="selectTab('all')">全部</button>
+      <button class="is-todo w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" @click="selectTab('已執行')">已執行</button>
+      <button class="not-todo w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" @click="selectTab('未執行')">未執行</button>
+    </div>
+    <table class="todo  border-4 text-2xl">
+      <thead>
+        <tr>
+          <th>執行</th>
+          <th>事項</th>
+          <th>功能</th>
+        </tr>
+      </thead>
+      <tbody class="data-show">
+        <tr v-for="(item, index) in selectnews" :key="item.id">
+          <th><input type="checkbox" v-model="item.checkBox" @change="changecheckbox()"></th>
+          <th v-if="item.msgSwitch === false">{{ item.news }}</th>
+          <th v-else><input type="text" @change="(e) => editInput(e)"></th>
+          <th>
+            <button class="enter-to-local w-16 rounded-lg text-2xl bg-red-50" type="button" @click="editBtn(index)">編輯</button>
+            <button class="enter-to-local w-16 rounded-lg text-2xl bg-red-50" type="button" @click="deleteBtn(index)">刪除</button>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+    
+
+  </div>
+</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,9 +7,21 @@ export default {
       matter: '',
       myObj: {},
       selectTab: '',
+      editText:'',
     }
   },
+  mounted(){
+    // 網頁開啟後先執行裡面JS
+    // 把資料從localStorage特定key值拿出來資料丟進msgArr裡面
+    // 清空資料
+    // localStorage.removeItem('msg');
+    if (localStorage.getItem('msg')) {
+      this.arr = JSON.parse(localStorage.getItem('msg'));
+    };
+  },
+  // 預處理拿到的資料,有暫存功能可將整個資料拿出來並利用判斷式將資料篩選
   computed: {
+    // 將陣列置換分頁內容對應陣列
     filterUseData() {
       if (!this.selectTab) {
         return this.arr;
@@ -20,58 +32,64 @@ export default {
   methods: {
     // 新增資料
     addData() {
-      if (this.matter != '') {
+      if (this.matter.trim() != '') {
         const id = this.arr.length > 0 ? this.arr[this.arr.length - 1].id : 0;
         this.myObj = {
             id: id + 1,
             matter: this.matter,
             check: false,
+            editSwitch:false,
           };
         this.arr.push(this.myObj);
       }
       this.matter = '';
+      localStorage.setItem('msg' , JSON.stringify(this.arr));
     },
-    addMatter(e) {
-      if (e.target.value != '') {
-        this.matter = e.target.value;
-        e.target.value = '';
-      }
-    },
-    doOrNot(e, index) {
-      this.arr[index].check = e.target.checked;
-    },
-    changeTab(string) {
-      this.selectTab = string;
-    },
+    // 查看checkBox是否被按下
+    // doOrNot(e, index) {
+    //   this.arr[index].check = e.target.checked;
+    //   localStorage.setItem('msg' , JSON.stringify(this.arr));
+    // },
+    // 刪除陣列元素
     deleteData(index) {
       this.arr.splice(index, 1);
+      localStorage.setItem('msg' , JSON.stringify(this.arr));
     },
-    editData(index) {
-      let userInput = prompt("請輸入事項:");
-      if (userInput === null || userInput === "") {
-        alert("請輸入內容！");
-      } else {
-        this.arr[index].matter = userInput;
-        alert('成功更換事項');
+    // 置換編輯文字與陣列文字
+    editData(item) {
+      item.editSwitch = !item.editSwitch;
+      if (item.editSwitch == false) {
+        item.matter = this.editText;
       }
+      else{
+        this.editText = item.matter;
+      };
+      localStorage.setItem('msg' , JSON.stringify(this.arr));
     },
+    // 讀取編輯文字
+    editMsg(e){
+      this.editText = e.target.value;
+    },
+    changeCheckBox(){
+      localStorage.setItem('msg' , JSON.stringify(this.arr));
+    }
   },
 }
 </script>
 <template>
-  <div class="w-3/5 m-auto h-full bg-gray-600">
+  <div class="w-3/5 m-auto h-full bg-gray-600 overflow-y-auto">
     <div class="input-item flex justify-center mt-3 border-b-2 pb-3">
-      <input type="text" class="w-1/3 pl-3 text-black" placeholder="請填寫事項(最多15字)" maxlength="15" @change="addMatter">
+      <input type="text" class="w-1/3 pl-3 text-black" placeholder="請填寫事項(最多15字)" maxlength="15" v-model="matter">
       <button class="ml-5 text-white hover:bg-slate-500 border p-1 rounded-xl" @click="addData()">
         新增</button>
     </div>
     <div class="flex mt-2 pb-2 border-b-2 gap-3">
       <button class="my-chooseTab-btn bg-green-300" :class="{ 'active': selectTab == '' }"
-        @click="changeTab('')">全部</button>
+        @click="() => selectTab = ''">全部</button>
       <button class="my-chooseTab-btn bg-green-300" :class="{ 'active': selectTab == 'true' }"
-        @click="changeTab('true')">已執行</button>
+        @click="() => selectTab = 'true'">已執行</button>
       <button class="my-chooseTab-btn bg-green-300" :class="{ 'active': selectTab == 'false' }"
-        @click="changeTab('false')">未執行</button>
+        @click="() => selectTab = 'false'">未執行</button>
     </div>
     <div>
       <table class="w-full pt-1 pb-2">
@@ -84,11 +102,11 @@ export default {
         </thead>
         <tbody>
           <tr v-for="(item, index) in filterUseData" :key="item.id">
-            <th class="my-tbody-th"><input type="checkbox" v-model="item.check" @change="(e) => doOrNot(e, index)"
-                class="w-5 h-5"></th>
-            <th class="my-tbody-th">{{ item.matter }}</th>
+            <th class="my-tbody-th"><input type="checkbox" v-model="item.check" class="w-5 h-5" @change="changeCheckBox()"></th>
+            <th class="my-tbody-th" v-if="item.editSwitch == false">{{ item.matter }}</th>
+            <th class="my-tbody-th" v-else><input type="text" class="text-black" @change="editMsg"></th>
             <th class="my-tbody-th">
-              <button class="my-th-btn" @click="editData(index)">編輯</button>
+              <button class="my-th-btn" @click="editData(item)">編輯</button>
               <button class="my-th-btn ml-3" @click="deleteData(index)">刪除</button>
             </th>
           </tr>
